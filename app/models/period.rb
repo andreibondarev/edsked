65d10f35b587e8d234e718721be1c242c498f293
Andrei Bondarev <abondarev@weddingwire.com>
2014-07-26 19:21:02 -0400

    New styles

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,8 @@
 class Period < ActiveRecord::Base
   has_many :users_periods
   has_many :users, through: :users_periods
+
+  validates :period_name, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
