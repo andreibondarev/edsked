4ca6449e161a2bc7ec4ae46a3fcccafd0f72d8ea
James Flowers <jflowers@weddingwire.com>
2014-07-26 22:52:57 -0400

    post_conference logic

--- app/controllers/bulk_controller.rb
+++ app/controllers/bulk_controller.rb
@@ -1,21 +1,28 @@
 class BulkController < ApplicationController
   before_filter :authenticate_user!
-  # POST /events
-  # POST /events.json
+
+  ### TODO, HANDLE SILENT ERRORS -- aka no time slots open
   def create
     bulk = bulk_params
 
     bulk[:users].each do |user|
-      date, period_id = match_period(user, bulk[:start_date], bulk[:end_date])
-      unless date.nil?
-        event = Event.create(event_type: :observation, event_date: date, period_id: period_id) 
-        EventsUser.create(user_id: current_user.id, event_id: event.id)
-        EventsUser.create(user_id: user.id, event_id: event.id)
-      end
+      event = create_event(user, start_date, end_date, :observation)
+      create_event(user, event.event_date, event.event_date+2.days, :post_conference)
     end
   end
 
   private
+    def create_event(user, start_date, end_date, type)
+      event = nil
+      date, period_id = match_period(user, start_date, start_date)
+      unless date.nil?
+        event = Event.create(event_type: type, event_date: date, period_id: period_id) 
+        EventsUser.create(user_id: current_user.id, event_id: event.id)
+        EventsUser.create(user_id: user.id, event_id: event.id)
+      end
+      return event
+    end
+
     def match_period(user, start_date, end_date)
       start_date..end_date.each |date|
         Period.all.each do |p|
